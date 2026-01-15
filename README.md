44308b9dd35e207f9c584dd371c06c75a645b562
José Manuel Calderón Palacios <jmanuel.canito@gmail.com>
2026-01-15 19:35:08 +0100

    Implementación de nueva funcionalidad y modificación del README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HelloWorld
+#José Manuel Calderon
--- src/com/git/helloworld/Main.java
+++ src/com/git/helloworld/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-
+		
+		int a = 5;
+		int b = 15;
+		System.out.println(a + b);
+		
 	}
 
 }
